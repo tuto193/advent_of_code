1e6190ec0304c9a50cfe7c59b1df66e5c16736fc
Carlos Alfonso Parra Fernandez <carlos.a.parra.f@gmail.com>
2022-12-23 23:20:46 +0100

    More progress

--- src/day_02.rs
+++ src/day_02.rs
@@ -7,4 +7,15 @@ pub fn day_02() -> usize {
     let wins = vec!["A Y", "B Z", "C X"];
     let losses = vec!["A Z", "B X", "C Y"];
     let contents = crate::get_file_contents("02".into());
+    let mut used_tactic_score = 0;
+    for i in contents.split("\n").into_iter() {
+
+        match i {
+            "A" => used_tactic_score += 1,
+            "B" => used_tactic_score += 2,
+            "C" => used_tactic_score += 3,
+
+        }
+    }
+    0
 }
