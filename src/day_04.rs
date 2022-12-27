9fceab0da75293980e996ad6bbeadb6ad34af8f2
Carlos Alfonso Parra Fernandez <carlos.a.parra.f@gmail.com>
2022-12-27 14:13:54 +0100

    Second check was wrong

--- src/day_04.rs
+++ src/day_04.rs
@@ -10,11 +10,15 @@ pub fn day_04() {
         }
         let elves = elf_pair.split(",").collect::<Vec<&str>>();
         if elves[0] == "" || elves[1] == "" {
-            print!("Skipped a non-empty line");
+            print!("Pair {} wasn't empty but after split it now is.", elf_pair);
             continue
         }
         let elf_1 = elves[0].split("-").collect::<Vec<&str>>();
         let elf_2 = elves[1].split("-").collect::<Vec<&str>>();
+        if elf_1.len() < 2 || elf_2.len() < 2 {
+            print!("Pair {:?} and {:?} wasn't empty but after split it now is.", elf_1, elf_2);
+            continue
+        }
         // parse into numbers
         let elf_1 = elf_1
             .into_iter()
@@ -24,25 +28,18 @@ pub fn day_04() {
             .into_iter()
             .map(|n| n.parse().unwrap())
             .collect::<Vec<usize>>();
-        let elf_1 = (elf_1[0]..elf_1[1]).collect::<Vec<usize>>();
-        let elf_2 = (elf_2[0]..elf_2[1]).collect::<Vec<usize>>();
-        if elf_1.iter().all(|t| elf_2.contains(t)) ||
-           elf_2.iter().all(|t| elf_1.contains(t)) {
-            println!("The elves \n{:?} and \n{:?} are self-contained", elf_1, elf_2);
-            total_contained += 1;
-        }
         // 2...1...|
-        // if elf_1[0] >= elf_2[0] {
-        //     // |...1...2
-        //     if elf_1[1] <= elf_2[1] {
-        //         total_contained += 1;
-        //     }
-        // }
-        // if elf_1[0] <= elf_2[0] {
-        //     if elf_1[1] >= elf_2[1] {
-        //         total_contained += 1;
-        //     }
-        // }
+        if elf_1[0] >= elf_2[0] {
+            // |...1...2
+            if elf_1[1] <= elf_2[1] {
+                total_contained += 1;
+            }
+        }
+        if elf_1[0] <= elf_2[0] {
+            if elf_1[1] >= elf_2[1] {
+                total_contained += 1;
+            }
+        }
     }
     println!("Total self-contained pairs: {}", total_contained);
 }
