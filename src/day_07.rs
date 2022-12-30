d2620afbe2bd2845952e996b5b90dc4149c3203b
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2022-12-30 15:27:00 +0100

    It finally works :D

--- src/day_07.rs
+++ src/day_07.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, ops::Index};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use crate::get_file_contents;
 
@@ -10,19 +10,17 @@ struct File {
 #[derive(Clone, Debug)]
 struct Directory {
     name: String,
-    // parent: Option<Box<Directory>>,
-    dirs: HashMap<String, Directory>,
+    parent: Option<Rc<RefCell<Directory>>>,
+    // Test
+    dirs: HashMap<String, Rc<RefCell<Directory>>>,
     files: Vec<File>,
 }
 
 impl Directory {
-    pub fn new(name: String) -> Self {
+    pub fn new(name: String, parent: Option<Rc<RefCell<Directory>>>) -> Self {
         Self {
             name: name,
-            // parent: match parent {
-            //     None => None,
-            //     Some(parent) => Some(Box::new(*parent)),
-            // },
+            parent: parent,
             dirs: HashMap::new(),
             files: vec![],
         }
@@ -36,26 +34,8 @@ impl Directory {
         for f in self.get_files().into_iter() {
             sum += f.size;
         }
-        // for d in s.iter() {
-        //     // let the_dir = dir_list.get(d).unwrap();
-        // }
-        // let my_dirs = updated_dirs
-        //     .iter()
-        //     .filter(|(n, &_)| self.dirs.contains_key(n.clone()))
-        //     .collect::<HashMap<&String, &Directory>>();
-        // for (_, d) in my_dirs.iter() {
-        // for (_n, d) in self.dirs.iter() {
-        //     sum += d.total_size();
-        // }
         sum
     }
-
-    // pub fn set_parent(mut self, p: Directory) {
-    //     match self.parent {
-    //         Some(n) => println!("Tried to set parent from {} to {}", n, p),
-    //         None => self.parent = Some(p.to_string()),
-    //     }
-    // }
 
     pub fn add_file(&mut self, size: usize, name: String) {
         let to_add = File {
@@ -65,32 +45,36 @@ impl Directory {
         self.files.push(to_add)
     }
 
-    pub fn add_dir(&mut self, dir: &str) {
-        // let my_clone = self.clone();
-        // let my_self_rc = Rc::new(self);
-        // let to_add = Directory::new(dir.to_string(), Some(&self.name));
-        let to_add = Directory::new(dir.to_string());
-        self.dirs.insert(to_add.name.clone(), to_add);
+    pub fn add_dir(&mut self, dir: Rc<RefCell<Directory>>) {
+        let name = &dir.borrow().name;
+        self.dirs.insert(name.clone(), Rc::clone(&dir));
     }
 
-    // pub fn get_parent(&self) -> &Box<Directory> {
-    //     // never use `..` on `/`
-    //     self.parent.as_ref().unwrap()
-    //     // to_show
-    // }
+    pub fn get_parent(&self) -> Option<Rc<RefCell<Directory>>> {
+        // never use `..` on `/`
+        match &self.parent {
+            None => {
+                println!("Tried to get parent of {}, but it's orphan.", self.name);
+                None
+            }
+            Some(p) => Some(Rc::clone(&p)),
+        }
+        // to_show
+    }
 
     /// Returns a vector of all files in this and all its subdirectories
     pub fn get_files(&self) -> Vec<File> {
         let mut to_return = self.files.clone();
         for (_, dir) in self.dirs.iter() {
             // println!("Dir '{}' is in {}", n, self.name);
-            let mut subdir_files = dir.get_files().into_iter().collect::<Vec<File>>();
+            let dir = Rc::clone(dir);
+            let mut subdir_files = dir.borrow_mut().get_files().into_iter().collect::<Vec<File>>();
             to_return.append(&mut subdir_files);
         }
         to_return
     }
 
-    pub fn get_dirs(&mut self) -> &mut HashMap<String, Directory> {
+    pub fn get_dirs(&mut self) -> &mut HashMap<String, Rc<RefCell<Directory>>> {
         &mut self.dirs
     }
 
@@ -101,7 +85,8 @@ impl Directory {
             total_sum += this_dirs_size;
         }
         for (_n, d) in self.dirs.iter() {
-            total_sum += d.get_all_under_limit(max_limit);
+            let d = Rc::clone(d);
+            total_sum += d.borrow_mut().get_all_under_limit(max_limit);
         }
         total_sum
     }
@@ -118,18 +103,20 @@ impl Directory {
             for _ in 0..depth * 4 {
                 print!("-")
             }
-                println!("++{} [{}]", f.name, f.size);
+            println!("++{} [{}]", f.name, f.size);
         }
         for (_n, d) in self.dirs.iter() {
-            d.print_hierarchy(depth + 1);
+            let d = Rc::clone(d);
+            d.borrow_mut().print_hierarchy(depth + 1);
         }
     }
 }
 
-fn get_directories_from_input(input: String) -> Vec<Directory> {
-    let mut dirs: Vec<Directory> = vec![];
+fn get_directories_from_input(input: String) -> Rc<RefCell<Directory>> {
+    // let mut dirs = vec![];
     let commands = input.split("$ ").collect::<Vec<&str>>();
-    let mut active_dirs: Vec<String> = vec![];
+    // let mut active_dirs: Vec<String> = vec![];
+    let mut active_directory = Rc::new(RefCell::new(Directory::new("/".to_string(), None)));
     for command in commands.into_iter().skip(1) {
         // println!("Command looks like ' {} '.", command);
         let lines = command.split("\n").collect::<Vec<&str>>();
@@ -142,23 +129,21 @@ fn get_directories_from_input(input: String) -> Vec<Directory> {
                 ".." => {
                     // go back to parent dir
                     // If this ever panics, it's because we tried to `..` at `/`
-                    let sub_dir_name = active_dirs.pop().unwrap();
-                    let sub_dir: Directory = dirs.pop().unwrap();
-                    // Update entry in Parent's HashMap
-                    let cur_dir: &mut Directory = dirs.last_mut().unwrap();
-                    cur_dir.get_dirs().remove(&sub_dir_name);
-                    cur_dir.get_dirs().insert(sub_dir_name.clone(), sub_dir);
+                    // TEST: Rcs and RefCells
+                    let parent_dir = active_directory.borrow_mut().get_parent().unwrap();
+                    // active_directory.
+                    active_directory = parent_dir;
                 }
+                "/" => continue,
                 dir_name => {
-                    // get into a new sub-directory (we found it through `ls`)
                     // println!("Directory name is '{}'", dir_name);
-                    let cur_dir: &mut Directory = dirs.last_mut().unwrap();
-                    let new_cur_dir = cur_dir.get_dirs().get_mut(dir_name).unwrap();
-                    // let new_dir = Directory::new(dir_name.to_string());
-                    // let already_there =
-                    active_dirs.push(dir_name.to_string());
-                    // let _already_there = dirs.insert(dir_name.to_string(), new_dir);
-                    dirs.push(*new_cur_dir);
+                    // Keep track of our current directory's name
+                    // active_dirs.push(dir_name.to_string());
+                    ///// TEST # 3: Rcs and Refcells
+                    let next_subdir = Rc::clone(&active_directory);
+                    let mut next_subdir = next_subdir.borrow_mut();
+                    let next_subdir = next_subdir.get_dirs().get(dir_name).unwrap();
+                    active_directory = Rc::clone(&next_subdir);
                 }
             }
         } else {
@@ -166,59 +151,48 @@ fn get_directories_from_input(input: String) -> Vec<Directory> {
             // Skip the `ls` part
             for line in lines.into_iter().skip(1) {
                 let output = line.split(" ").collect::<Vec<&str>>();
-                let active_dir= dirs.last_mut().unwrap();
+                // let active_dir = dirs.last_mut().unwrap();
                 match output[0] {
                     "dir" => {
-                        // let cur_dir = mut_active_dir.clone();
                         let found_dir_name = output[1];
-                        println!("Dir '{}': found dir '{}'", active_dirs[0], found_dir_name);
-                        // let to_add_dir = Directory::new(found_dir_name.to_string(), Some(mut_active_dir));
-                        // let active_dir = active_dirs.last().unwrap();
-                        active_dir.get_dirs().insert(found_dir_name.to_string(), Directory::new(found_dir_name.to_string()));
-                        println!("Dir '{}': added dir '{}'", active_dir.name, output[1]);
+                        // println!("Dir '{}': added dir '{}'", active_dir.name, output[1]);
+                        // Test: Rcs and RefCells
+                        let found_child_dir = Rc::new(RefCell::new(Directory::new(found_dir_name.to_string(), Some(Rc::clone(&active_directory)))));
+                        active_directory.borrow_mut().add_dir(found_child_dir);
                     }
                     "" => continue,
                     f_size => {
-                        // println!("File '{}'\t is '{}'", output[1], f_size);
                         let file_size: usize = f_size.parse().unwrap();
-                        // let mut actual_mut_active_dir = mut_active_dir.deref();
-                        // let active_dir = active_dirs.last().unwrap();
-                        active_dir.add_file(file_size, output[1].to_string());
+                        active_directory.borrow_mut().add_file(file_size, output[1].to_string());
                         // println!("Dir '{}': added file '{}'", active_dir.name, output[1]);
                     }
                 }
             }
         }
     }
-    dirs
+    // dirs
+    loop {
+        {
+            let my_clone = Rc::clone(&active_directory);
+            match my_clone.borrow_mut().get_parent() {
+                None => break,
+                Some(_p) => {
+                    // let parent = Rc::clone(&active_directory);
+                }
+            };
+        }
+        // active_directory = active_directory.borrow_mut().get_parent().unwrap();
+        let parent = active_directory.borrow_mut().get_parent().unwrap();
+        active_directory = Rc::clone(&parent);
+    }
+    active_directory
 }
 
 pub fn day_07() {
-    // let input = get_file_contents("07".to_string());
-    let input = get_file_contents("007".to_string());
-    let directories = get_directories_from_input(input);
+    let input = get_file_contents("07".to_string());
+    let root = get_directories_from_input(input);
     let max_size: usize = 100000;
-    let root = directories.index(0);
-    // root.print_hierarchy(1);
-    for d in directories.iter() {
-        d.print_hierarchy(1);
-    }
-    let total_sum = root.get_all_under_limit(max_size);
-    // let directories_wanted = directories.iter().filter(|(_, d)| {
-    //     // println!("Dir {} contains {:?}", nd.1.name, nd.1.get_files());
-    //     // println!("Dir {} total size {}", nd.1.name, nd.1.total_size());
-    //     d.total_size(&directories) <= max_size
-    // });
-    // let directories_unwanted = directories.iter().filter(|(_, d)| {
-    //     // println!("Dir {} contains {:?}", nd.1.name, nd.1.get_files());
-    //     // println!("Dir {} total size {}", nd.1.name, nd.1.total_size());
-    //     d.total_size(&directories) > max_size
-    // });
-    // for (n, d) in directories_unwanted {
-    //     println!("Dir '{}' \t has total size '{}'", n, d.total_size(&directories));
-    // }
-    // for (_, d) in directories_wanted {
-    //     total_sum += d.total_size(&directories);
-    // }
+    // root.borrow_mut().print_hierarchy(1);
+    let total_sum = root.borrow_mut().get_all_under_limit(max_size);
     println!("The total sum of all under {} is: {}", max_size, total_sum);
 }
