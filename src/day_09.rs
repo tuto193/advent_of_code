388da38e0fa3d7687a56b4dcf3bfcbaeee568e86
Carlos Alfonso Parra Fernandez <carlos.a.parra.f@gmail.com>
2023-01-02 01:18:20 +0100

    Day 9 part 1 done

--- src/day_09.rs
+++ src/day_09.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, ops, hash::Hash};
+use std::{collections::HashSet, ops::{self}, hash::Hash, vec};
 
 use crate::get_file_contents;
 
@@ -14,12 +14,13 @@ enum Direction {
     Right,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-struct Position {
-    x: i32,
-    y: i32,
-}
+// #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+// struct Position {
+//     x: i32,
+//     y: i32,
+// }
 
+type Position = (i32, i32);
 #[derive(Clone, Debug)]
 struct Tail {
     position: Position,
@@ -27,91 +28,102 @@ struct Tail {
     visited_position: Vec<Position>,
 }
 
-impl ops::Add for Position {
-    type Output = Position;
-    fn add(self, rhs: Position) -> Position {
-        Position {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
-    }
+// impl ops::Add for Position {
+//     type Output = Position;
+//     fn add(self, rhs: Position) -> Position {
+//         Position{
+//             x: self.0 + rhs.0,
+//             y: self.1 + rhs.1,
+//         }
+//     }
+// }
+
+// impl ops::Sub for Position {
+//     type Output = Self;
+
+//     fn sub(self, other: Self) -> Self {
+//         Self {
+//             x: self.0 - other.0,
+//             y: self.1 - other.1,
+//         }
+//     }
+// }
+
+fn is_adjacent(this: Position, other: Position) -> bool {
+    let vector = (this.0 - other.0, this.1 - other.1);
+    // vector.0.abs() + vector.1.abs() == 1 && (this.0 == other.0 || this.1 == other.1)
+    let up = get_direction(Direction::Up);
+    let down = get_direction(Direction::Down);
+    let left = get_direction(Direction::Left);
+    let right = get_direction(Direction::Right);
+    vector == up || vector == down || vector == left || vector == right
 }
 
-impl ops::Sub for Position {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
+fn get_direction(dir: Direction) -> Position {
+    let (mut x, mut y) = (0, 0);
+    match dir {
+        Direction::Up => y = -1,
+        Direction::Down => y = 1,
+        Direction::Left => x = -1,
+        Direction::Right => x = 1,
+        Direction::UpLeft => {
+            x = -1;
+            y = -1;
+        }
+        Direction::UpRight => {
+            x = 1;
+            y = -1;
+        }
+        Direction::DownRight => {
+            x = 1;
+            y = 1;
+        }
+        Direction::DownLeft => {
+            x = -1;
+            y = 1;
         }
     }
+
+    (x, y)
 }
 
-impl Position {
-    pub fn is_adjacent(&self, other: Self) -> bool {
-        self.x == other.x || self.y == other.y
-    }
-
-    pub fn get_direction(dir: Direction) -> Self {
-        let (mut x, mut y) = (0, 0);
-        match dir {
-            Direction::Up => y = -1,
-            Direction::Down => y = 1,
-            Direction::Left => x = -1,
-            Direction::Right => x = 1,
-            Direction::UpLeft => {
-                x = -1;
-                y = -1;
-            }
-            Direction::UpRight => {
-                x = 1;
-                y = -1;
-            }
-            Direction::DownRight => {
-                x = 1;
-                y = 1;
-            }
-            Direction::DownLeft => {
-                x = -1;
-                y = 1;
-            }
-        }
-
-        Self{x, y}
-    }
-
-    pub fn is_diagonal(&self, other: Self) -> bool {
-        let vector = *self - other;
-        !self.is_adjacent(other) && vector.x != 0 && vector.y != 0
-    }
+fn is_diagonal(this: Position, other: Position) -> bool {
+    let vector = (this.0 - other.0, this.1 - other.1);
+    !is_adjacent(this, other) && vector.0.abs() == 1 && vector.1.abs() == 1
 }
 
 impl Tail {
     pub fn new() -> Self {
         Self {
-            position: Position { x: 0, y: 0 },
-            head: Position { x: 0, y: 0 },
-            visited_position: vec![Position{x: 0, y: 0}],
+            position: (0, 0),
+            head: (0, 0),
+            visited_position: vec![(0, 0)],
         }
     }
 
     pub fn move_head(&mut self, direction: Direction, steps: usize) {
-        println!("Moving");
         if steps == 0 {
             return;
         }
         // Head always moves
         let old_head = self.head;
-        let new_head = self.head + Position::get_direction(direction);
+        let dir = get_direction(direction);
+        let new_head = (self.head.0 + dir.0, self.head.1 + dir.1);
         self.head = new_head;
+        // let on_top = self.position == self.head;
+        // let is_adjacent = self.position.is_adjacent(self.head);
+        // let is_diagonal = self.position.is_diagonal(self.head);
+        // println!("On top? {} \nAdjacent? {} \nDiagonal? {}",on_top, is_adjacent, is_diagonal);
         // Head either is or was on top
-        if self.position == self.head || self.position.is_adjacent(self.head) || self.position.is_diagonal(self.head) {
+        if self.position == self.head || // Head moved onto tail
+            is_adjacent(self.position, self.head) || // head moved somewhere near tail
+            is_diagonal(self.position, self.head) { // Head moved somewhere diagonal to tail
             // We do nothing
-            println!("")
+            // println!("Head moved, but tail didn't");
         } else {
-            self.position = old_head.clone();
-            self.visited_position.push(old_head);
+            // println!("Tail moved from {:?} to {:?}", self.position, old_head);
+            self.position = old_head;
+            self.visited_position.push(self.position);
         }
         // Mimic what the head does
         self.move_head(direction, steps - 1);
@@ -134,8 +146,8 @@ pub fn day_09() {
             "D" => Direction::Down,
             "L" => Direction::Left,
             "R" => Direction::Right,
-            _ => {
-                println!("Moved into an unexpected direction");
+            x => {
+                println!("Moved into an unexpected direction: '{}'", x);
                 continue;
             }
         };
