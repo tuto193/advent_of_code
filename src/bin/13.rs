5f2724f8260cd8435fdeaaf94c1719b98298d60a
Carlos Alfonso Parra Fernandez <carlos.a.parra.f@gmail.com>
2023-01-11 00:36:59 +0100

    Added some basis. Continue tomorrow

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,8 +1,36 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+fn is_right_bigger_than_left(left: &str, right: &str) -> bool {
+        let mut left = left.split(&['[', ',', ']'][..]).into_iter();
+        let mut right = right.split(&['[', ',', ']'][..]).into_iter();
+        let mut last_left = left.next();
+        let mut last_right = right.next();
+        'numbers: loop {
+            if let Some(left) = last_left {
+                break 'numbers;
+            }
+        }
+    false
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+fn get_right_orders_sum(signal: Vec<&str>) -> usize {
+    let mut sum = 0;
+    for (i, packet) in signal.into_iter().enumerate() {
+        let l_r: Vec<&str> = packet.split("\n").collect();
+        if is_right_bigger_than_left(l_r[0], l_r[1]) {
+            sum += i + 1;
+        }
+    }
+    sum
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let signal: Vec<&str> = input.split("\n\n").collect();
+    let total_packets = signal.len() - 1;
+    let signal: Vec<&str> = signal.into_iter().take(total_packets).collect();
+    let sum_of_right_orders = get_right_orders_sum(signal);
+    return Some(sum_of_right_orders);
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
     None
 }
 
@@ -19,7 +47,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 13);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(13));
     }
 
     #[test]
