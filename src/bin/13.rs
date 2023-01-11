663444a20f6b84816b2f7789da56f5ccd7da265f
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2023-01-11 16:21:45 +0100

    Let's carry on from the bed

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,14 +1,96 @@
 fn is_right_bigger_than_left(left: &str, right: &str) -> bool {
-        let mut left = left.split(&['[', ',', ']'][..]).into_iter();
-        let mut right = right.split(&['[', ',', ']'][..]).into_iter();
+        let mut left = left.split(&['[', ','][..]).into_iter();
+        let mut right = right.split(&['[', ','][..]).into_iter();
         let mut last_left = left.next();
         let mut last_right = right.next();
         'numbers: loop {
-            if let Some(left) = last_left {
-                break 'numbers;
+            // println!("Comparing L:{:?} and R:{:?}", last_left, last_right);
+            if let Some(this_left) = last_left {
+                // Left still has stuff, so compare
+                match this_left {
+                    "" => {
+                        // Left is "[" so advance it
+                        last_left = left.next();
+                        if let Some(this_right) = last_right {
+                            // right is standing on something
+                            match this_right {
+                                "" => last_right = right.next(),
+                                right_number_par=> {
+                                    let actual_right: &str = right_number_par
+                                        .split("]")
+                                        .into_iter().next().unwrap();
+                                    match actual_right {
+                                        "" => {
+                                            // It was an empty list, so left is bigger
+                                            return false;
+                                        }
+                                        num => {
+                                            // It's standing on a number, so we can carry on passing it on
+                                            last_right = Some(num);
+                                        }
+                                    }
+                                }
+                            }
+                            continue 'numbers;
+                        } else {
+                            // Right ran out before left. Out of order!
+                            return false;
+                        }
+                    }
+                    // Left: on number or number+"]+"
+                    left_num_par => {
+                        let actual_left = left_num_par
+                            .split("]")
+                            .into_iter()
+                            .next()
+                            .unwrap();
+                        match actual_left {
+                            "" => {
+                                // It's empty, so it was just an empty list in the end
+                                if let Some(this_right) = last_right {
+                                    let actual_right = this_right.split("]").into_iter().next().unwrap();
+                                    match actual_right {
+                                        "" => {
+                                            // Right also doesn't go deeper
+                                            // con
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if let Some(this_right) = last_right {
+                            match this_right{
+                                "" => {
+                                    last_right = right.next();
+                                    continue 'numbers;
+                                }
+                                right_num => {
+                                    let l_num: isize = left_num.parse().unwrap();
+                                    let r_num: isize = right_num.parse().unwrap();
+                                    if r_num == l_num {
+                                        // they're equal
+                                        last_left = left.next();
+                                        last_right = right.next();
+                                        continue 'numbers;
+                                    } else {
+                                        return r_num > l_num;
+                                    }
+                                }
+                            }
+                        } else {
+                            // left had a number, but right had run out.
+                            return false;
+                        }
+                    }
+                }
+            } else if let Some(_this_right) = last_right {
+                // right has more, and left doesn't, so it's the right oder
+                return true;
+            } else {
+                // Both ran out while still being "equal", so it's not the right oder
+                return false;
             }
         }
-    false
 }
 
 fn get_right_orders_sum(signal: Vec<&str>) -> usize {
@@ -16,6 +98,7 @@ fn get_right_orders_sum(signal: Vec<&str>) -> usize {
     for (i, packet) in signal.into_iter().enumerate() {
         let l_r: Vec<&str> = packet.split("\n").collect();
         if is_right_bigger_than_left(l_r[0], l_r[1]) {
+            // println!("Right order. Adding!");
             sum += i + 1;
         }
     }
