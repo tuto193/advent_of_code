dd59b5187bd72f9628da4c6a9946a27d63a4c18d
Carlos Alfonso Parra Fernandez <carlos.a.parra.f@gmail.com>
2023-01-18 22:38:47 +0100

    Carry on later

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,4 +1,38 @@
+fn init_level(height: u32, width: u32) -> Vec<Vec<bool>> {
+    let mut to_ret = vec![];
+    for _ in 0..height {
+        let mut to_append = vec![];
+        for _ in 0..width {
+            to_append.push(false);
+        }
+        to_ret.push(to_append);
+    }
+    to_ret
+}
+
+fn draw_wall(level: &mut Vec<Vec<bool>>, lines: &str) {
+    let coords_list: Vec<(u32, u32)> = lines
+        .split(" -> ")
+        .into_iter()
+        .map(|l| {
+            l
+                .split(",")
+
+
+        })
+}
+
+fn parse_level(input: &str) -> Vec<Vec<bool>> {
+    let mut level = init_level(1000, 1000);
+    for wall_lines in input.split("\n") {
+        draw_wall(&mut level, wall_lines);
+    }
+    level
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut level = init_level(1000, 1000);
+
     None
 }
 
