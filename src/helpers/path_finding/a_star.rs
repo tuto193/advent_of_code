c06b8ee0005ed30b458d4462d94cf2b472c9dba5
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2023-02-06 01:09:21 +0100

    need to make it in A star or maybe Eulerian Path :/

--- src/helpers/path_finding/a_star.rs
+++ src/helpers/path_finding/a_star.rs
@@ -1,5 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 type Valve = (String, usize, Vec<String>);
 
-pub fn reconstruct_path(came_from: HashMap<String, Valve>, current: String)
+pub fn reconstruct_path(came_from: VecDeque<Valve>, current: String) -> VecDeque<String> {
+    let mut total_path = VecDeque::new();
+    total_path.push_front(current);
+    while let Some(cur)
+    total_path
+}
